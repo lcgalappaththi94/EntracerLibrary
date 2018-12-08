7f4762e502dbe2bb1469ec52118d5f279649e316
Lachithra Chathura Galappaththi <l.chathura.galappaththi@gmail.com>
2018-12-08 09:43:37 +0530

    Latest release with Async Task Test getPersonByID

--- entracerlibrary/src/main/java/entracer/chinthika/com/entracerlibrary/EntracerLib.java
+++ entracerlibrary/src/main/java/entracer/chinthika/com/entracerlibrary/EntracerLib.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
@@ -31,12 +33,35 @@ public class EntracerLib {
         executor = Executors.newFixedThreadPool(10);
     }
 
-    public String getPerson(String id) {
-        return "The response";
-    }
-
     private String getTag() {
         return UUID.randomUUID().toString();
+    }
+
+    public Person getPersonById(String id) {
+        final String tag = getTag();
+        final String url = new StringBuffer("http://crm.orete.org/api/v1/people/").append(id).toString();
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .addHeader("Authorization", new StringBuffer("Token token=").append(token).toString())
+                            .get()
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    concurrentHashMap.put(tag, response.body().string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    concurrentHashMap.put(tag, e.toString());
+                }
+            }
+        });
+
+        while (!concurrentHashMap.containsKey(tag)) ;
+        String result = concurrentHashMap.get(tag);
+        Gson gson = new Gson();
+        return gson.fromJson(result,Person.class);
     }
 
     public String getAllPersons() {
@@ -62,6 +87,8 @@ public class EntracerLib {
 
         while (!concurrentHashMap.containsKey(tag)) ;
         String result = concurrentHashMap.get(tag);
+        Gson gson = new Gson();
+        gson.fromJson(result,Person.class);
         return result;
     }
 
--- entracerlibrary/src/main/java/entracer/chinthika/com/entracerlibrary/Person.java
+++ entracerlibrary/src/main/java/entracer/chinthika/com/entracerlibrary/Person.java
@@ -36,4 +36,259 @@ public class Person {
     private Date created_at;
     private Date updated_at;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDo_not_call() {
+        return do_not_call;
+    }
+
+    public void setDo_not_call(String do_not_call) {
+        this.do_not_call = do_not_call;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public String getReferred_by() {
+        return referred_by;
+    }
+
+    public void setReferred_by(String referred_by) {
+        this.referred_by = referred_by;
+    }
+
+    public String getTarget_value() {
+        return target_value;
+    }
+
+    public void setTarget_value(String target_value) {
+        this.target_value = target_value;
+    }
+
+    public String getAltenative_email() {
+        return altenative_email;
+    }
+
+    public void setAltenative_email(String altenative_email) {
+        this.altenative_email = altenative_email;
+    }
+
+    public String getAltenative_phone() {
+        return altenative_phone;
+    }
+
+    public void setAltenative_phone(String altenative_phone) {
+        this.altenative_phone = altenative_phone;
+    }
+
+    public String getStreet_1() {
+        return street_1;
+    }
+
+    public void setStreet_1(String street_1) {
+        this.street_1 = street_1;
+    }
+
+    public String getStreet_2() {
+        return street_2;
+    }
+
+    public void setStreet_2(String street_2) {
+        this.street_2 = street_2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getSkype() {
+        return skype;
+    }
+
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Person assignee) {
+        this.assignee = assignee;
+    }
+
+    public Organization[] getOrganizations() {
+        return organizations;
+    }
+
+    public void setOrganizations(Organization[] organizations) {
+        this.organizations = organizations;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
 }
